68e0bcb0ccf6cd7beb23eef28f0f8d4fce061912
irpanzy <irfanoputra27@gmail.com>
2026-07-24 21:42:33 +0700

    refactor(config): standardize exports to named exports across config modules

--- src/config/imagekit.ts
+++ src/config/imagekit.ts
@@ -1,10 +1,8 @@
 import ImageKit from "imagekit";
 import { env } from "./env";
 
-const imagekit = new ImageKit({
+export const imagekit = new ImageKit({
   publicKey: env.IMAGEKIT_PUBLIC_KEY,
   privateKey: env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: env.IMAGEKIT_URL_ENDPOINT,
 });
-
-export default imagekit;
